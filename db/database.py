91d27e17c03491d3ba3c40f5f902f5dcd2e40a35
Timmyy3000 <timiogunme@gmail.com>
2023-03-08 23:47:44 +0100

    new push

--- db/database.py
+++ db/database.py
@@ -3,6 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
+from dotenv  import load_dotenv
+
+
+load_dotenv()
+
 DATABASE_URL = os.environ["PROD_DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
