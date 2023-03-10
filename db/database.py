7f99bd48a96010bf095928a0c9c3685b8331f6cc
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2023-03-10 17:22:16 +0100

    fix: switch to cloud mysql

--- db/database.py
+++ db/database.py
@@ -1,17 +1,35 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from google.cloud.sql.connector import Connector, IPTypes
 
-# DATABASE_URL = os.environ["PROD_DATABASE_URL"]
-DATABASE_URL = "sqlite:///./billing-prod.db"
+db_host = os.environ["INSTANCE_HOST"]  # e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+db_user = os.environ["DB_USER"]  # e.g. 'my-db-user'
+db_pass = os.environ["DB_PASS"]  # e.g. 'my-db-password'
+db_name = os.environ["DB_NAME"]  # e.g. 'my-database'
+db_port = os.environ["DB_PORT"]  # e.g. 3306
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Python Connector database connection function
+def getconn():
+    with Connector() as connector:
+        conn = connector.connect(
+            "billing-380023:us-central1:colligate-prod", # Cloud SQL Instance Connection Name
+            "pymysql",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type= IPTypes.PUBLIC  # IPTypes.PRIVATE for private IP
+        )
+    return conn
+
+DATABASE_URL = "mysql+pymysql://"
+
+engine = create_engine(DATABASE_URL, creator=getconn)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
