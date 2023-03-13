ffe16e61f0d3684b8237db414038a7da607c3a28
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2023-03-13 14:42:43 +0100

    fix: switch dev database to cloud sql

--- db/database.py
+++ db/database.py
@@ -2,16 +2,36 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from google.cloud.sql.connector import Connector, IPTypes
 
-# DATABASE_URL = os.environ["PROD_DATABASE_URL"]
-DATABASE_URL = "sqlite:///./billing-prod.db"
+db_host = os.environ["INSTANCE_HOST"]  # e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+db_user = os.environ["DB_USER"]  # e.g. 'my-db-user'
+db_pass = os.environ["DB_PASS"]  # e.g. 'my-db-password'
+db_name = os.environ["DB_NAME"]  # e.g. 'my-database'
+db_port = os.environ["DB_PORT"]  # e.g. 3306
+connection_name = os.environ['CONNECTION_NAME']
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Python Connector database connection function
+def getconn():
+    with Connector() as connector:
+        conn = connector.connect(
+            connection_name, # Cloud SQL Instance Connection Name
+            "pymysql",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type= IPTypes.PUBLIC  # IPTypes.PRIVATE for private IP
+        )
+    return conn
+
+DATABASE_URL = "mysql+pymysql://"
+
+engine = create_engine(DATABASE_URL, creator=getconn)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
@@ -19,3 +39,20 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# # DATABASE_URL = os.environ["PROD_DATABASE_URL"]
+# DATABASE_URL = "sqlite:///./billing-prod.db"
+
+# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
+
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
